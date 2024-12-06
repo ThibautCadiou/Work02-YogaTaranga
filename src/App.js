43f95dc6b9d4a77b8d05b1c9021faf3b37ccc8e8
ThibautCadiou <cadioucontact@gmail.com>
2024-12-06 14:42:50 +0100

    ajouts de la partie actualités

--- src/App.js
+++ src/App.js
@@ -9,11 +9,36 @@ export default function App() {
       <main className='main'>
         <Activites />
         <section className='actualites u-center-text'>
-          <h2 className='heading-section'>Actualités</h2>
+          <h2 className='heading-section actualites__title'>Actualités</h2>
+
           <div className='actualites__container'>
-            <div className='actualites__items actualites__items--1'></div>
-            <div className='actualites__items actualites__items--2'></div>
-            <div className='actualites__items actualites__items--3'></div>
+            <div className='actualites__items actualites__items--1 actualite'>
+              <img src='./img/home-02.jpg' alt='atelier meditation ' className='actualite__img' />
+              <p className='actualite__tag tag'>Méditation</p>
+              <h3 className='actualite__title'>Nos Prochains Ateliers Méditations</h3>
+              <h4 className='actualite__subtitle'>(Saison2024-2025)</h4>
+              <button className='btn btn--actualites btn--white actualite__btn'>
+                En savoir plus
+              </button>
+            </div>
+            <div className='actualites__items actualites__items--2 actualite'>
+              <img src='./img/home-03.jpg' alt='atelier meditation ' className='actualite__img' />
+              <p className='actualite__tag tag color-2'>Yoga</p>
+              <h3 className='actualite__title'>Nos prochaines retraites</h3>
+              <h4 className='actualite__subtitle'>(Printemps 2025)</h4>
+              <button className='btn btn--actualites btn--white actualite__btn color-2'>
+                En savoir plus
+              </button>
+            </div>
+            <div className='actualites__items actualites__items--1 actualite'>
+              <img src='./img/home-04.jpg' alt='atelier meditation ' className='actualite__img' />
+              <p className='actualite__tag tag'>Articles</p>
+              <h3 className='actualite__title'>Nos derniers articles</h3>
+              <h4 className='actualite__subtitle'>Chandra Yoga & Traditions Siddhas</h4>
+              <button className='btn btn--actualites btn--white actualite__btn'>
+                En savoir plus
+              </button>
+            </div>
           </div>
         </section>
         <section className='decouvrir u-center-text'>
